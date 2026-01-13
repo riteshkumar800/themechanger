b004c2df274dc74e894124a2d88aec382c7534d3
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-01-13 23:07:15 +0530

    code

--- themechanger/src/App.jsx
+++ themechanger/src/App.jsx
@@ -2,6 +2,16 @@
 import './App.css'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
   
   return (
     <>
